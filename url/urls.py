51c8cfa0e5de50354b641e9acfc3eed5aa13b0ce
Oscardk <dauren2050@yandex.ru>
2024-05-21 13:55:23 +0500

    add register

--- url/urls.py
+++ url/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
    path('url/<str:hash>/', views.redirect_original_url),
    path('url/', views.create_short_url),
    path('url/stats/<str:hash>/', views.get_url_stats),
+   path('signup/', views.signup, name='signup'),
+   path('login/', views.login_view, name='login'),
+   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+   #path('', views.index, name='index'),
 ]
--- url/views.py
+++ url/views.py
@@ -6,6 +6,10 @@ from django.http import JsonResponse
 import hashlib
 from .serializers import URLSerializer
 from rest_framework.response import Response
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignUpForm
+
 
 def redirect_original_url(request, hash):
     try:
@@ -47,3 +51,34 @@ def simple_ui(request):
     urls = URL.objects.all()
     ## Render template
     return render(request, "index.html", {"urls": urls})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def index(request):
+    return render(request, 'index.html')
